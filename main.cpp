d2c3595cb596e161820ed2405b412d18b88588fb
Martin Rosselle <rosselm@gmail.com>
2022-11-01 16:28:26 +0100

    got pigpio library linked

--- main.cpp
+++ main.cpp
@@ -1,3 +1,35 @@
 #include <iostream>
+#include <limits>
+#include <string>
+#include <numbers>
+// #include "src/gpio/gpio.h"
+// #include <format> // not available
+#include <pigpio.h>
 
-int main() { std::cout << "yo, cross compiled" << std::endl; }
+
+// testing c++20 compilation
+consteval int sqr(int n) {
+  return n*n;
+}
+constexpr int r = sqr(100); // OK
+int counter {0};
+long long distance {15'000'000LL};
+
+const wchar_t *txt = L"Hello World";
+std::wstring s = std::wstring(txt);
+
+auto pi(std::numbers::pi);
+
+int main() { 
+    std::cout << "yo, cross compiled and btw, the sqr of 100 is " << r << std::endl;
+    std::wcout << "yes this is a  wstring : "  << s << std::endl;
+    //std::cout << std::numeric_limits<short>::min();
+    std::cout << "Hello Pi (" << pi << ")" << std::endl;
+
+    // try out the gpio class
+    // gpio::GPIO input26 = gpio::GPIO(26);
+
+    if (gpioInitialise() < 0) {
+    return 1; // Failed to initialize.
+  }
+}
